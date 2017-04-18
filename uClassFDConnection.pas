59749e501337cbc505b7e4e3ef96a48c8b6153d2
André Luis <dolby1games@gmail.com>
2017-04-17 21:24:22 -0300

    Criado conexão com o Banco de Dados

---
+++ uClassFDConnection.pas
@@ -0,0 +1,16 @@
+unit uClassFDConnection;
+
+interface
+
+uses
+  FireDAC.Comp.Client, FireDAC.Comp.UI;
+
+type
+  TFDConnection = class(TFDConnection)
+  private
+
+  end;
+
+implementation
+
+end.
