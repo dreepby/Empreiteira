6c97185b93f2474146074bb4a1da87a8fc3eba25
André Luis <dolby1games@gmail.com>
2017-04-25 00:17:59 -0300

    Criação listagem base
    Não está pronto

---
+++ uListagemBase.pas
@@ -0,0 +1,34 @@
+unit uListagemBase;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.Buttons,
+  Data.DB, Vcl.Grids, Vcl.DBGrids, System.ImageList, Vcl.ImgList;
+
+type
+  TfrmListagemBase = class(TForm)
+    Panel1: TPanel;
+    Label1: TLabel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    btnFechar: TBitBtn;
+    frmExcluir: TBitBtn;
+    BitBtn1: TBitBtn;
+    btnCadastrar: TBitBtn;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmListagemBase: TfrmListagemBase;
+
+implementation
+
+{$R *.dfm}
+
+end.
