002d7d1c41cf53aa1468fe7d402618b3f1cac08a
André Luis <dolby1games@gmail.com>
2017-05-11 21:42:10 -0300

    Revert "Adionaod uma nova propriedade"
    This reverts commit d0fbfb91f7f759e0c01c8f4d835735c4618a4fed.

--- uMunicipioDto.pas
+++ uMunicipioDto.pas
@@ -8,17 +8,14 @@ type
     FidMunicipio: Integer;
     FidUf: integer;
     FNome: String;
-    FnomeUf: String;
     procedure SetidMunicipio(const Value: Integer);
     procedure SetidUf(const Value: integer);
     procedure SetNome(const Value: String);
-    procedure SetnomeUf(const Value: String);
 
   public
     property idMunicipio: Integer read FidMunicipio write SetidMunicipio;
     property Nome: String read FNome write SetNome;
     property idUf: integer read FidUf write SetidUf;
-    property nomeUf: String read FnomeUf write SetnomeUf;
   end;
 
 implementation
@@ -38,11 +35,6 @@ end;
 procedure TMunicipioDto.SetNome(const Value: String);
 begin
   FNome := Value;
-end;
-
-procedure TMunicipioDto.SetnomeUf(const Value: String);
-begin
-  FnomeUf := Value;
 end;
 
 end.
