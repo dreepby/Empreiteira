4f23414f2b7f7b0bd257d29f038e93984c1e7a36
André Luis <dolby1games@gmail.com>
2017-05-15 19:30:27 -0300

    Alteração propriedade idMunicipio

--- uBairrosDto.pas
+++ uBairrosDto.pas
@@ -5,18 +5,18 @@ interface
 type
   TBairrosDto = class
   private
-    FidMunicipio: String;
     FNomeMunicipio: String;
     FidBairro: Integer;
     FNome: String;
+    FidMunicipio: Integer;
     procedure SetidBairro(const Value: Integer);
-    procedure SetidMunicipio(const Value: String);
     procedure SetNome(const Value: String);
     procedure SetNomeMunicipio(const Value: String);
+    procedure SetidMunicipio(const Value: Integer);
   public
     property idBairro: Integer read FidBairro write SetidBairro;
     property Nome: String read FNome write SetNome;
-    property idMunicipio: String read FidMunicipio write SetidMunicipio;
+    property idMunicipio: Integer read FidMunicipio write SetidMunicipio;
     property NomeMunicipio: String read FNomeMunicipio write SetNomeMunicipio;
   end;
 
@@ -29,7 +29,8 @@ begin
   FidBairro := Value;
 end;
 
-procedure TBairrosDto.SetidMunicipio(const Value: String);
+
+procedure TBairrosDto.SetidMunicipio(const Value: Integer);
 begin
   FidMunicipio := Value;
 end;
