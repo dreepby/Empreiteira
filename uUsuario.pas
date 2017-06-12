ca1bac36e433797b6fa51d2b6a1c17569a3a3a76
Samoel Dewes <dwssamuca@gmail.com>
2017-06-12 19:36:11 -0300

    Validação de dados
    iniciando

--- uUsuario.pas
+++ uUsuario.pas
@@ -5,12 +5,13 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, uCadastroBase, Data.DB, Vcl.Buttons,
-  Vcl.Grids, Vcl.DBGrids, Vcl.ComCtrls, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Grids, Vcl.DBGrids, Vcl.ComCtrls, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Mask;
 
 type
   TfrmUsuario = class(TfrmCadastroBase)
     edtNome: TLabeledEdit;
-    edtCPF: TLabeledEdit;
+    edtCPF: TMaskEdit;
+    Label2: TLabel;
   private
     { Private declarations }
   public
